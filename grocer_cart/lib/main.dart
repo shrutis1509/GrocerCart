eab82a1f182efa2fb75c33f53ee6b868a16053f9
Prachi-Verma018 <prachiverma711@gmail.com>
2023-02-28 11:09:08 +0530

    added comment

--- grocer_cart/lib/main.dart
+++ grocer_cart/lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         //hello
+        //buffelo
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
