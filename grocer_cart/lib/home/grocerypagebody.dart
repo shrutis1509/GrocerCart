142b8c968b04129d9f18a2212162c8e83a157b1d
Nancygarg12 <nancygara2@gmail.com>
2023-03-11 21:00:33 +0530

    upto1hr:08 min

--- grocer_cart/lib/home/grocerypagebody.dart
+++ grocer_cart/lib/home/grocerypagebody.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:grocer_cart/widgets/bigtext.dart';
 
 class GroceryPageBody extends StatefulWidget {
   const GroceryPageBody({Key? key}) : super(key: key);
@@ -9,36 +11,73 @@ class GroceryPageBody extends StatefulWidget {
 }
 
 class _GroceryPageBodyState extends State<GroceryPageBody> {
+  PageController pageController=PageController( viewportFraction: 0.85);
   @override
   Widget build(BuildContext context) {
+
     return Container(
+      color:Colors.redAccent,
       height: 320,
       child:PageView.builder(
-        itemCount: 6,
-        itemBuilder:(context,position){
-        return _buildPageItem(position);
-    }),
-
+        controller: pageController,
+          itemCount: 6,
+          itemBuilder:(context,position){
+            return _buildPageItem(position);
+          }),
     );
   }
   Widget _buildPageItem(int index){
-    return Container(
+    return Stack(
+      children: [
+        Container(
+          height:220,
+          margin: EdgeInsets.only(left:6,right:6),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color:  index.isEven?Color(0xFFFFCC80):Color(0xFFFFCA28),
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image:AssetImage(
+                      "assets/image/gain.jpg"
+                  )
+              )
 
-      height:220,
-      margin: EdgeInsets.only(left:6,right:6),
-    decoration: BoxDecoration(
-    borderRadius: BorderRadius.circular(30),
-    color:  index.isEven?Color(0xFFFFCC80):Color(0xFFFFCA28),
-      image: DecorationImage(
-        fit: BoxFit.cover,
-        image:AssetImage(
-          "assets/image/gain.jpg"
+          ),
+        ),
+
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height:140,
+            margin: EdgeInsets.only(left:30,right:30,bottom: 15),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color:  Colors.white,
+
+            ),
+            child: Container(
+              padding: EdgeInsets.only(top:15,left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: "Soyabean"),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(5, (index) => Icon(Icons.star, size: 15,)),
+
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ),
         )
-      )
-
-      ),
+      ],
     );
-
-
   }
 }
+
+
