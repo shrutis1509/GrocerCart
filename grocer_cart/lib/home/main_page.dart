5e0d1d73e49be87724b136291b276af7fe9c2a47
Shruti <sinha.shruti0915@gmail.com>
2023-03-15 14:13:00 +0530

    fixed a few more config issues

--- grocer_cart/lib/home/main_page.dart
+++ grocer_cart/lib/home/main_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:grocer_cart/widgets/bigtext.dart';
 import 'package:grocer_cart/widgets/smalltext.dart';
